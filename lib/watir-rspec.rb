f7189905cdcc9ae5701ef7549648bc1ec76a709f
Jarmo Pertman <jarmo.p@gmail.com>
2012-08-25 15:49:58 +0300

    Initial commit.

---
+++ lib/watir-rspec.rb
@@ -0,0 +1,7 @@
+require "watir-rspec/version"
+
+module Watir
+  module Rspec
+    # Your code goes here...
+  end
+end
---
+++ lib/watir-rspec/version.rb
@@ -0,0 +1,5 @@
+module Watir
+  module Rspec
+    VERSION = "0.0.1"
+  end
+end

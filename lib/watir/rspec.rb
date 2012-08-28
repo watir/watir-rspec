1882c4a43bb1a7507c3504d02d395c9eb9a6b5c1
Jarmo Pertman <jarmo.p@gmail.com>
2012-08-28 17:33:14 +0300

    Rename bootstrap method name to something more meaningful.

--- lib/watir/rspec.rb
+++ lib/watir/rspec.rb
@@ -25,134 +25,134 @@ module Watir
   #
   class RSpec
     class << self
-      def bootstrap const
-        const.class_eval do
+        def add_within_and_during_to_matcher const
+          const.class_eval do
 
-          inst_methods = instance_methods.map &:to_sym
+            inst_methods = instance_methods.map &:to_sym
 
-          if !(inst_methods.include?(:__matches?) || inst_methods.include?(:__does_not_match?)) && 
-            (inst_methods.include?(:matches?) || inst_methods.include?(:does_not_match?))
+            if !(inst_methods.include?(:__matches?) || inst_methods.include?(:__does_not_match?)) && 
+              (inst_methods.include?(:matches?) || inst_methods.include?(:does_not_match?))
 
-            def within(timeout)
-              @within_timeout = timeout
-              self
+              def within(timeout)
+                @within_timeout = timeout
+                self
+              end
+
+              def during(timeout)
+                @during_timeout = timeout
+                self
+              end
+
+              def soon 
+                within(30)
+              end      
+
+              def seconds
+                # for syntactic sugar
+                self
+              end
+
+              alias_method :second, :seconds
+
+              def minutes
+                @within_timeout *= 60 if @within_timeout
+                @during_timeout *= 60 if @during_timeout
+                self
+              end
+
+              alias_method :minute, :minutes
             end
 
-            def during(timeout)
-              @during_timeout = timeout
-              self
+            if inst_methods.include? :matches?
+              alias_method :__matches?, :matches? 
+
+              def matches?(actual)
+                match_with_wait {__matches?(actual)}
+              end
             end
 
-            def soon 
-              within(30)
-            end      
+            if inst_methods.include? :does_not_match?
+              alias_method :__does_not_match?, :does_not_match?
 
-            def seconds
-              # for syntactic sugar
-              self
+              def does_not_match?(actual)
+                match_with_wait {__does_not_match?(actual)}
+              end
+            elsif inst_methods.include? :matches?
+              def does_not_match?(actual)
+                match_with_wait {!__matches?(actual)}
+              end
             end
 
-            alias_method :second, :seconds
+            private
 
-            def minutes
-              @within_timeout *= 60 if @within_timeout
-              @during_timeout *= 60 if @during_timeout
-              self
-            end
-
-            alias_method :minute, :minutes
-          end
-
-          if inst_methods.include? :matches?
-            alias_method :__matches?, :matches? 
-
-            def matches?(actual)
-              match_with_wait {__matches?(actual)}
-            end
-          end
-
-          if inst_methods.include? :does_not_match?
-            alias_method :__does_not_match?, :does_not_match?
-
-            def does_not_match?(actual)
-              match_with_wait {__does_not_match?(actual)}
-            end
-          elsif inst_methods.include? :matches?
-            def does_not_match?(actual)
-              match_with_wait {!__matches?(actual)}
-            end
-          end
-
-          private
-
-          def match_with_wait
-            if @within_timeout
-              timeout = @within_timeout; @within_timeout = nil
-              Watir::Wait.until(timeout) {yield} rescue false
-            elsif @during_timeout
-              timeout = @during_timeout; @during_timeout = nil
-              Watir::Wait.while(timeout) {yield} rescue true        
-            else
-              yield
+            def match_with_wait
+              if @within_timeout
+                timeout = @within_timeout; @within_timeout = nil
+                Watir::Wait.until(timeout) {yield} rescue false
+              elsif @during_timeout
+                timeout = @during_timeout; @during_timeout = nil
+                Watir::Wait.while(timeout) {yield} rescue true        
+              else
+                yield
+              end
             end
           end
         end
-      end
 
-      def file_path(file_name, description=nil)
-        formatter.file_path(file_name, description=nil)
-      end
+        def file_path(file_name, description=nil)
+          formatter.file_path(file_name, description=nil)
+        end
 
-      private
+        private
 
-      def formatter
-        @formatter ||= begin
-                         formatter = ::RSpec.configuration.formatters.find {|f| f.kind_of? Watir::RSpec::HtmlFormatter}
-                         unless formatter
-                           raise <<-EOF
-Watir::RSpec::HtmlFormatter is not set as a RSpec formatter.
+        def formatter
+          @formatter ||= begin
+                           formatter = ::RSpec.configuration.formatters.find {|f| f.kind_of? Watir::RSpec::HtmlFormatter}
+                           unless formatter
+                             raise <<-EOF
+  Watir::RSpec::HtmlFormatter is not set as a RSpec formatter.
 
-You need to add it into your spec_helper.rb file like this:
-  RSpec.configure do |config|
-    config.add_formatter('documentation')
-    config.add_formatter(Watir::RSpec::HtmlFormatter)
-  end
-EOF
+  You need to add it into your spec_helper.rb file like this:
+    RSpec.configure do |config|
+      config.add_formatter('documentation')
+      config.add_formatter(Watir::RSpec::HtmlFormatter)
+    end
+  EOF
+                           end
+                           formatter
                          end
-                         formatter
-                       end
+        end
+
+
       end
 
-
-    end
-
-  end
-end
-
-# patch for #within(timeout) method
-module ::RSpec::Matchers
-  class BuiltIn::Change
-    def matches?(event_proc)
-      raise_block_syntax_error if block_given?
-
-      # to make #change work with #in(timeout) method
-      unless defined? @actual_before
-        @actual_before = evaluate_value_proc
-        event_proc.call
-      end
-      @actual_after = evaluate_value_proc
-
-      (!change_expected? || changed?) && matches_before? && matches_after? && matches_expected_delta? && matches_min? && matches_max?
     end
   end
 
-  alias_method :make, :change
-end
+  # patch for #within(timeout) method
+  module ::RSpec::Matchers
+    class BuiltIn::Change
+      def matches?(event_proc)
+        raise_block_syntax_error if block_given?
 
-matchers = RSpec::Matchers::BuiltIn.constants.map(&:to_sym)
-matchers.delete :BaseMatcher
-matchers.each do |const|
-  Watir::RSpec.bootstrap RSpec::Matchers::BuiltIn.const_get const
-end
+        # to make #change work with #in(timeout) method
+        unless defined? @actual_before
+          @actual_before = evaluate_value_proc
+          event_proc.call
+        end
+        @actual_after = evaluate_value_proc
 
-Watir::RSpec.bootstrap RSpec::Matchers::DSL::Matcher
+        (!change_expected? || changed?) && matches_before? && matches_after? && matches_expected_delta? && matches_min? && matches_max?
+      end
+    end
+
+    alias_method :make, :change
+  end
+
+  matchers = RSpec::Matchers::BuiltIn.constants.map(&:to_sym)
+  matchers.delete :BaseMatcher
+  matchers.each do |const|
+    Watir::RSpec.add_within_and_during_to_matcher RSpec::Matchers::BuiltIn.const_get const
+  end
+
+Watir::RSpec.add_within_and_during_to_matcher RSpec::Matchers::DSL::Matcher

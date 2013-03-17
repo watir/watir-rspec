268970df16037e2ee7958c76aa7db0a829c80dfe
Jarmo Pertman <jarmo.p@gmail.com>
2013-03-17 22:22:49 +0200

    Retrieve ActiveRecord shared connection lazily. Fixes #1.

--- lib/watir/rspec/active_record_shared_connection.rb
+++ lib/watir/rspec/active_record_shared_connection.rb
@@ -1,16 +1,18 @@
+require "thread"
+
 RSpec.configuration.before(:suite) do
   # Allow RSpec specs to use transactional fixtures when using Watir. 
   #
   # Tip from http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
   class ::ActiveRecord::Base
-    mattr_accessor :shared_connection
+    @@shared_connection_semaphore = Mutex.new
     @@shared_connection = nil
 
     def self.connection
-      @@shared_connection || retrieve_connection
+      @@shared_connection_semaphore.synchronize do
+        @@shared_connection ||= retrieve_connection
+      end
     end
   end
 
-  # Forces all threads to share the same connection. 
-  ::ActiveRecord::Base.shared_connection = ::ActiveRecord::Base.connection
 end

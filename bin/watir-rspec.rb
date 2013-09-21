0f322fb89e223774a04868e4970ecf1665e028ba
Jarmo Pertman <jarmo.p@gmail.com>
2013-09-21 16:24:32 +0300

    Add watir-rspec binary for installation.

---
+++ bin/watir-rspec.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+require "watir/rspec/cli"
+Watir::RSpec::CLI.execute
+
---
+++ lib/watir/rspec/cli.rb
@@ -0,0 +1,110 @@
+require "optparse"
+
+module Watir
+  class RSpec
+    class CLI
+      class << self
+        def execute
+          @options = {}
+          parser = OptionParser.new do |opts|
+            opts.banner "Install watir-rspec configuration into spec_helper file.
+Usage: watir-rspec [options] install"
+
+            opts.on("-r", "--[no-]rails", "Force to install watir-rspec for Rails application if the detection fails") do |v|
+              @options[:rails] = v
+            end
+          end
+
+          parser.parse!
+
+          if ARGV.size != 1 || ARGV.first != "install"
+            puts parser
+            exit
+          end
+
+          install
+        end
+
+        private
+
+        def install
+          spec_helper = File.join(spec_directory, "spec_helper.rb")
+          puts "Rails application #{using_rails? ? "detected" : "not detected"}.
+Installing watir-rspec configuration into #{spec_helper)}."
+
+          File.open(spec_helper, "a") do |file|
+            file.puts %Q[
+
+# Configuration for watir-rspec
+require "watir/rspec"
+
+RSpec.configure do |config|
+  # Use Watir::RSpec::HtmlFormatter to get links to the screenshots, html and
+  # all other files created during the failing examples.
+  config.add_formatter(:progress) if config.formatters.empty?
+  config.add_formatter(Watir::RSpec::HtmlFormatter)
+
+  # Open up the browser for each example.
+  config.before :all#{rails_filter} do
+    @browser = Watir::Browser.new
+  end
+
+  # Close that browser after each example.
+  config.after :all#{rails_filter} do
+    @browser.close if @browser
+  end
+
+  # Include RSpec::Helper into each of your example group for making it possible to
+  # write in your examples instead of:
+  #   @browser.goto "localhost"
+  #   @browser.text_field(:name => "first_name").set "Bob"
+  #
+  # like this:
+  #   goto "localhost"
+  #   text_field(:name => "first_name").set "Bob"
+  #
+  # This needs that you've used @browser as an instance variable name in
+  # before :all block.
+  config.include Watir::RSpec::Helper#{rails_filter}
+
+  # Include RSpec::Matchers into each of your example group for making it possible to
+  # use #within with some of RSpec matchers for easier asynchronous testing:
+  #   @browser.text_field(:name => "first_name").should exist.within(2)
+  #   @browser.text_field(:name => "first_name").should be_present.within(2)
+  #   @browser.text_field(:name => "first_name").should be_visible.within(2)
+  #
+  # You can also use #during to test if something stays the same during the specified period:
+  #   @browser.text_field(:name => "first_name").should exist.during(2)
+  config.include Watir::RSpec::Matchers#{rails_filter}
+end
+            ]
+          end
+        end
+
+        def spec_directory
+          specs = File.expand_path("spec", File.dirname(__FILE__))
+
+          unless File.directory? specs
+            puts %Q[#{specs} directory not found.
+Make sure you run the watir-rspec command within your projects' directory.]
+
+            exit 1
+          end
+
+          specs
+        end
+
+        def rails_filter
+          ", :type => :request" if using_rails?
+        end
+
+        def using_rails?
+          @options[:rails] || @using_rails ||= begin
+                                                 File.exists?("Gemfile.lock") && File.read("Gemfile.lock") =~ /rspec-rails/ ||
+                                                   File.exists?("Gemfile") && File.read("Gemfile") =~ /rspec-rails/
+                                               end
+        end
+      end
+    end
+  end
+end

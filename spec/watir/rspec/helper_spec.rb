3121465f99abdab0f74cdf5b3a8dee0cead3dc13
Jarmo Pertman <jarmo.p@gmail.com>
2015-03-21 12:55:12 +0200

    Improve specs.

--- spec/watir/rspec/helper_spec.rb
+++ spec/watir/rspec/helper_spec.rb
@@ -20,6 +20,24 @@ describe Watir::RSpec::Helper do
       $browser = :global_browser
       expect(browser).to be $browser
     end
+
+    context "methods with block" do
+      before { @browser = double("browser") }
+
+      it "are delegated to browser" do
+        expect(@browser).to receive(:window).with(title: "my_window") { |&block| block.call }
+
+        block_parameter = lambda { "my block content" }
+        expect(window(title: "my_window", &block_parameter)).to be == "my block content"
+      end
+
+      it "having parameters are properly delegated to browser" do
+        expect(@browser).to receive(:window).with(:title => "my_window").and_yield("first", "second")
+
+        block_parameter = lambda { |parameter_1, parameter_2| [parameter_1, parameter_2] }
+        expect(window(title: "my_window", &block_parameter)).to be == ["first", "second"]
+      end
+    end
   end
 
   context "#method_missing" do
@@ -52,36 +70,5 @@ describe Watir::RSpec::Helper do
     expect(described_class).not_to receive(:method_missing)
 
     expect(p).to eql "#p"
-  end
-
-  context "Block parameter for browser method" do
-    before :each do
-      @browser = double("browser")
-    end
-
-    it "block content is propagated to browser method when method is first called" do
-      expect(@browser).to receive(:window).with(:title => "my_window") { |&block| expect(block.call).to be == "my block content" }
-      # expect(described_class).to receive(:method_missing)
-      window(:title => "my_window") { "my block content" }
-    end
-
-    it "block content is propagated to browser method when method was already called" do
-      expect(@browser).to receive(:window).with(:title => "my_window") { |&block| expect(block.call).to be == "my block content" }.twice
-      # expect(described_class).to receive(:method_missing)
-      window(:title => "my_window") { "my block content" }
-      # expect(described_class).not_to receive(:method_missing)
-      window(:title => "my_window") { "my block content" }
-    end
-
-    it "Correct number of block parameters are properly propagated to browser method when method is first called" do
-      expect(@browser).to receive(:window).with(:title => "my_window").and_yield("first block parameter content", "second block parameter content")
-      expect(window(:title => "my_window") { |parameter_1, parameter_2| parameter_1 + " & " + parameter_2 }).to be == "first block parameter content & second block parameter content"
-    end
-
-    it "Correct number of block parameters are properly propagated to browser method when method is first called" do
-      expect(@browser).to receive(:window).with(:title => "my_window").and_yield("first block parameter content", "second block parameter content").twice
-      expect(window(:title => "my_window") { |parameter_1, parameter_2| parameter_1 + " & " + parameter_2 }).to be == "first block parameter content & second block parameter content"
-      expect(window(:title => "my_window") { |parameter_1, parameter_2| parameter_1 + " & " + parameter_2 }).to be == "first block parameter content & second block parameter content"
-    end
   end
 end
